e1a5e9f95f5fb54c27549151bf90d0fb51b1ac4b
deshwal28 <deshwalshab65@gmail.com>
2025-02-01 08:17:22 +0530

    first commit

---
+++ Day1/index.js
@@ -0,0 +1,28 @@
+// var age = 21;
+// if(true){
+//     console.log(age);
+// }
+// function solve(){
+//     var age =21;
+//     console.log(age);
+
+// }
+// //console.log(age);
+// solve();
+// var x = 10;
+// var x =20;
+// {
+// let a = 10 ;
+// console.log(a);
+// }
+// let a = 20;
+// a = "Deshwal";
+// a=true;
+// a=null;
+
+// const a = 28;
+// console.log(a);
+let marks = 20.234;
+marks="Deshwal";
+marks = true;
+console.log(marks);
