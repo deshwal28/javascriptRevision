e75d154ddb21b8db63d63c1b9e607f7741a7d2a7
deshwal28 <deshwalshab65@gmail.com>
2025-02-03 12:17:47 +0530

    commit

---
+++ Day1/Day2/index.js
@@ -0,0 +1,87 @@
+//airthmetic operator
+
+//let a = 10;
+//let b = 5;
+
+//console.log(a+b);
+//console.log(a*b);
+//console.log(a/b);
+//console.log(a%b);
+//console.log(a**b);
+
+//unaryOperator
+//++a
+//--b
+
+//Assigment Operator 
+//let a = 5 ;
+//example : a = a+10; similar to a+=10; And a = a-6; similar to a -= 6;
+
+//Comparission operator (>, < ,>=,<=,==,===)
+//  console.log('5'==5);//loose equality
+//  console.log('5'===5);//strict equality
+
+ //logical operator AND , OR , NOT 
+ 
+//  let ans =(true && false && true )
+//let ans =(true && true && true )
+// let ans =(false || true||false )
+// let ans =!(true)
+//  console.log(ans);
+
+//-------------------------------------------------WORKING WITH NON BOOLEANS---------------------------------------------------
+//FALSEY 
+//TRUTHY
+
+
+//BITWISE OPERATOR 
+//console.log(2||5);
+//console.log(~(0));
+
+//console.log(10<<1);
+
+//CONDITIONALS
+// let age = 3;
+// if(age >= 18) {
+//     console.log('can vote');
+// }
+// else{
+//     console.log('cannot vote');
+// }
+
+// let number = 5;
+// if(number == 1){
+//     console.log('A');
+
+// }
+// else if (number == 2){
+//     console.log('B' );
+// }
+// else if ( number == 3){
+//     console.log('C');
+// }
+// else if (number == 4){
+//     console.log('D' );
+// }
+// else if (number == 5){
+//     console.log('E' );
+// }
+// else{
+//     console.log('Not found ')
+// }
+
+//SWITCH CASE 
+let num = 3 ; 
+switch(num){
+    case 1:console.log('A')
+    break;
+    case 2:console.log('B')
+    break;
+    case 3:console.log('C')
+    break;
+    case 4:console.log('D')
+    break;
+    case 5:console.log('E')
+    break;
+    default:console.log('F')
+}
